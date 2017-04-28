d16a14e196526c4c0f6ad84c97b9ef15c0b5efa6
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-04-28 14:02:32 +0100

    add cinema.sql schema

---
+++ db/cinema.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS tickets;
+DROP TABLE IF EXISTS films;
+CREATE TABLE customers (
+  id SERIAL4 PRIMARY KEY,
+  name VARCHAR(255),
+  funds NUMERIC(2)
+);
+
+CREATE TABLE films(
+  id SERIAL4 PRIMARY KEY,
+  title VARCHAR(255),
+  price NUMERIC(2)
+);
+
+CREATE TABLE tickets(
+  id SERIAL4 PRIMARY KEY,
+  customer_id INT2 REFERENCES customers(id) ON DELETE CASCADE,
+  film_id INT2 REFERENCES films(id) ON DELETE CASCADE
+);
