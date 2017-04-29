12792d6aa32c8a1888852e7ee8b68fbfc0ede7e5
Daniel Garrido <daniel.garrido.info@gmail.com>
2017-04-29 12:24:06 +0100

    add save and update methods for Ticket class and add Ticket timestamp to sql schema

--- db/console.rb
+++ db/console.rb
@@ -7,9 +7,18 @@ require_relative('../db/sql_runner.rb')
 
 customer1 = Customer.new({"name" => "Daniel", 'funds' => 100.2})
 customer2 = Customer.new({"name" => "Maria", 'funds' => 55.5})
+customer1.save()
+customer2.save()
 
 film1 = Film.new({"title" => "X men", "price" => 4.5})
 film2 = Film.new({"title" => "Batman", "price" => 14.99})
+film1.save()
+film2.save()
+
+ticket1 = Ticket.new( {"film_id" => film1.id, "customer_id" => customer1.id} )
+ticket2 = Ticket.new( {"film_id" => film2.id, "customer_id" => customer2.id} )
+ticket1.save()
+ticket2.save()
 
 binding.pry
 nil
--- models/ticket.rb
+++ models/ticket.rb
@@ -2,9 +2,34 @@
 
 class Ticket
 
+  attr_reader :id
+  attr_accessor :customer_id, :film_id
+
   def initialize(params)
-    @id = params['id']
+    @id = params['id'] if params['id']
     @customer_id = params['customer_id']
     @film_id = params['film_id']
+  end
+
+  def save()
+    sql = "
+    INSERT INTO tickets
+    (customer_id, film_id)
+    VALUES
+    (#{@customer_id}, #{@film_id})
+    RETURNING id;
+    "
+    result = SqlRunner.run(sql)
+    @id = result.first['id'].to_i
+  end
+
+  def update()
+    sql = "
+    UPDATE tickets SET
+    (customer_id, film_id) =
+    (#{@customer_id}, #{@film_id})
+    WHERE id = #{@id};
+    "
+    SqlRunner.run(sql)
   end
 end
